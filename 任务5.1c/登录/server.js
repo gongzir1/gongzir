4f91e77c6a34c9054c8e557452efa4df7effdcb2
gongzir1 <87960255+gongzir1@users.noreply.github.com>
2021-09-10 15:59:12 +0800

    update server.js

--- 任务5.1c/登录/server.js
+++ 任务5.1c/登录/server.js
@@ -1,56 +1,64 @@
 const express = require("express")
 const app = express()
+const bodyParser = require("body-parser")
 const https=require("https")
+//连接数据库
+let mongoose=require('mongoose')
+mongoose.set('useCreateIndex',true)
 const bcrypt =require('bcrypt')
 
-
 app.use(express.static(__dirname+'/'))
+app.use('/model', express.static("model"))
 //引入模型对象，进行CRUD
-const userModel=require('./mongodb/model/user')
+const userModel=require('./model/user')
 //引入数据库模块
-const db=require('./mongodb/model/db')
+const db=require('./model/db')
 const { json, response } = require("express")
+const { LEGAL_TCP_SOCKET_OPTIONS } = require("mongodb")
 //使用内置中间件用于解析post请求的urlencoded参数
 app.use(express.urlencoded({extended:true}))
-db(()=>{
-    app.get('/',(req,res)=>{
+app.use(bodyParser.urlencoded({extended:true}))
+const uri = "mongodb+srv://gongzir:Gzr12345@cluster0.a9jqo.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+app.use('/public', express.static("public"))
+    app.get('/register',(req,res)=>{
         res.sendFile(__dirname+"/public/register.html")
        })
-    app.post('/',async (req,res)=>{
+    app.post('/register',async(req,res)=>{
         //获取用户输入
         // console.log("it is ok")
       //  res.send("okkk")
       const salt=await bcrypt.genSalt(10)
        const hashedPassword=await bcrypt.hash(req.body.password,salt)
         // console.log(hashedPassword)
-      console.log(req.body)
+     // console.log(req.body)
         const{country,firstname,lastname,email,password,re_password,address,city,state,zip,phone}=req.body
-        const data={
-            members:[{
-                email_address:email,
-                status:"subscribed",
-                merge_fields:{
-                    FNAME:firstname,
-                    LNAME:lastname
-                }
-            }]
-        }
-        jsonData=JSON.stringify(data)
-        // const apiKey="6838db82fdee020273afb2813258257b-us5"
-        // const list_id="5e9b7302c6"
-        const url="https://us5.api.mailchimp.com/3.0/lists/5e9b7302c6"
-        const options={
-            method:"POST",
-            auth:"azi:6838db82fdee020273afb2813258257b-us5"
-        }
-       const request= https.request(url,options,(response)=>{
-            response.on("data",(data)=>{
-                console.log(JSON.parse(data))
-            })
-        })
-        request.write(jsonData)
-        request.end()
-        console.log(firstname,lastname,email)
+    //     const data={
+    //         members:[{
+    //             email_address:email,
+    //             status:"subscribed",
+    //             merge_fields:{
+    //                 FNAME:firstname,
+    //                 LNAME:lastname
+    //             }
+    //         }]
+    //     }
+    //     jsonData=JSON.stringify(data)
+    //     // const apiKey="6838db82fdee020273afb2813258257b-us5"
+    //     // const list_id="5e9b7302c6"
+    //     const url="https://us5.api.mailchimp.com/3.0/lists/5e9b7302c6"
+    //     const options={
+    //         method:"POST",
+    //         auth:"azi:6838db82fdee020273afb2813258257b-us5"
+    //     }
+    //    const request= https.request(url,options,(response)=>{
+    //         response.on("data",(data)=>{
+    //             console.log(JSON.parse(data))
+    //         })
+    //     })
+    //     request.write(jsonData)
+    //     request.end()
+    //     console.log(firstname,lastname,email)
         //校验数据合法性：
         const passwordReg=/^[a-zA-Z0-9_@]{8,20}$/
         if(country==''){
@@ -80,11 +88,12 @@ db(()=>{
                 }else{
                     userModel.create({country,firstname,lastname,email,hashedPassword,address,city,state,zip,phone},function(err,data){
                         if(!err){
-                            //  res.send('successfully!')
-                            res.redirect('/login')
+                             // res.send('successfully!')
+                             // res.redirect('/login')
+                              res.sendFile(__dirname+"/public/login.html")
 
 
-var send = require('./mail.js');
+// var send = require('./mail.js');
 // var sendmail='<div> <a href="https://mailchi.mp/102ae4f6309b/welcome?e=[UNIQID]">You have registered iServer successfully! click to know more!</a><div>'
 // // 创建一个邮件对象
 // var mail = {
@@ -128,11 +137,11 @@ var send = require('./mail.js');
         //  校验失败：提示用户
     
     })
-    app.get('/',(req,res)=>{
+    app.get('/login',(req,res)=>{
         res.sendFile(__dirname+"/public/login.html")
        })
     //登录请求
-    app.post('/',async(req,res)=>{
+    app.post('/login',async(req,res)=>{
         //正则
         const passwordReg=/^[a-zA-Z0-9_@]{8,20}$/
         //校验数据
@@ -192,10 +201,21 @@ var send = require('./mail.js');
       port = 3000;
     }
     app.listen(port);
-},
+
     (err)=>{
         console.log(err)
-    })
-
-
-    
+    }
+    // if (process.env.NODE_ENV === 'production') {
+    //     // Exprees will serve up production assets
+    //     app.use(express.static(__dirname + '/public'));
+      
+    //     // Express serve up index.html file if it doesn't recognize route
+    //     const path = require('path');
+    //     app.get('*', (req, res) => {
+    //       res.sendFile(path.resolve(__dirname,'public','register.html'));
+    //     });
+    //     app.get('*', (req, res) => {
+    //         res.sendFile(path.resolve(__dirname,'public','login.html'));
+    //       });
+    //   }
+   
